d603ef90fdc20a2a54a2297f398a8e9eea75562b
arnaud chevallier <chevallier.a0@gmail.com>
2022-12-21 19:20:50 +0100

    deploy

--- src/api/base.js
+++ src/api/base.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-let BASE_URL  = "https://www.budget-react-app.online"
+let BASE_URL  = "https://budget-react-app.onrender.com"
 
 if(process.env.NODE_ENV !== 'production') {
   BASE_URL = `http://localhost:3005`
